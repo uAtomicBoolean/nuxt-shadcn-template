81228784a53df5751c0fa0ef2f7f04f0449e3581
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-10-22 10:24:09 +0200

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+	<div class="h-screen w-screen flex flex-row flex-wrap items-center justify-center gap-4">
+		<Button>Button</Button>
+		<Button variant="secondary">Button</Button>
+	</div>
+</template>
