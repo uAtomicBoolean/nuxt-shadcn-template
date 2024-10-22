3a7c0b09f44742530ec21402957c63748012bfc1
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2024-10-22 10:55:47 +0200

    chore: Switch to CSS variables to easily  modify the theme.

--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,9 @@
 	<div class="h-screen w-screen flex flex-row flex-wrap items-center justify-center gap-4">
 		<Button>Button</Button>
 		<Button variant="secondary">Button</Button>
+		<Button variant="destructive">Button</Button>
+		<Button variant="outline">Button</Button>
+		<Button variant="ghost">Button</Button>
+		<Button variant="link">Button</Button>
 	</div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 import * as animate from 'tailwindcss-animate';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
 	darkMode: ['class'],
-	content: [
-		'./pages/**/*.{ts,tsx,vue}',
-		'./components/**/*.{ts,tsx,vue}',
-		'./app/**/*.{ts,tsx,vue}',
-		'./src/**/*.{ts,tsx,vue}',
-	],
+	safelist: ['dark'],
 	prefix: '',
 	theme: {
 		container: {
@@ -19,6 +14,47 @@ module.exports = {
 			},
 		},
 		extend: {
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))',
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))',
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			borderRadius: {
+				xl: 'calc(var(--radius) + 4px)',
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+			},
 			keyframes: {
 				'accordion-down': {
 					from: { height: 0 },
@@ -28,10 +64,20 @@ module.exports = {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: 0 },
 				},
+				'collapsible-down': {
+					from: { height: 0 },
+					to: { height: 'var(--radix-collapsible-content-height)' },
+				},
+				'collapsible-up': {
+					from: { height: 'var(--radix-collapsible-content-height)' },
+					to: { height: 0 },
+				},
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
+				'collapsible-down': 'collapsible-down 0.2s ease-in-out',
+				'collapsible-up': 'collapsible-up 0.2s ease-in-out',
 			},
 		},
 	},
